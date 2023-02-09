b40bbb53550341dc4f47ab8225e21f82af195d7a
Yuri Padilla <yuripadilla017@gmail.com>
2023-02-09 20:52:01 +0100

    update userstory8

--- components/ProductDetails/index.js
+++ components/ProductDetails/index.js
@@ -1,17 +1,55 @@
-import Image from "next/image";
+import {
+  StyledImage,
+  StyledWrapper,
+  StyledPreviousNextLink,
+  StyledPreviousNext,
+} from "./ProductDetails.styled";
+import SVGIcon from "../SVGIcon";
+import { bikes } from "../../lib/bikes";
 
 export default function ProductDetails({ product }) {
+  const productIndex = bikes.findIndex((bike) => {
+    return bike.id === product.id;
+  });
+
+  console.log(bikes[productIndex]);
+
   return (
     <>
       <h2>Details</h2>
       <section>
-        <Image
+        <StyledImage
           src={product.imageSource}
           height={240}
           width={400}
           alt={product.marke}
           priority
         />
+        <StyledWrapper>
+          {productIndex > 0 ? (
+            <StyledPreviousNextLink
+              href={`/Bikes/${bikes[productIndex - 1].id}`}
+            >
+              <SVGIcon variant="previous" width="50px" color="black" />
+            </StyledPreviousNextLink>
+          ) : (
+            <StyledPreviousNext>
+              <SVGIcon variant="previous" width="50px" color="#acacac" />
+            </StyledPreviousNext>
+          )}
+
+          {productIndex < bikes.length - 1 ? (
+            <StyledPreviousNextLink
+              href={`/Bikes/${bikes[productIndex + 1].id}`}
+            >
+              <SVGIcon variant="next" width="50px" color="black" />
+            </StyledPreviousNextLink>
+          ) : (
+            <StyledPreviousNext>
+              <SVGIcon variant="next" width="50px" color="#acacac" />
+            </StyledPreviousNext>
+          )}
+        </StyledWrapper>
         <article>
           <p>
             <strong>Mark:</strong> {product.marke}
--- components/SVGIcon/index.js
+++ components/SVGIcon/index.js
@@ -36,6 +36,14 @@ const icons = {
     path: "M2,21L23,12L2,3V10L17,12L2,14V21Z",
     viewbox: "0 0 24 24",
   },
+  previous: {
+    path: "M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z",
+    viewbox: "0 0 24 24",
+  },
+  next: {
+    path: "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z",
+    viewbox: "0 0 24 24",
+  },
 };
 
 export default function SVGIcon({ variant, width, color = "currentColor" }) {
